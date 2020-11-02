919dfb5762e53b0a55d2dd9cd780f4a06aeb5edc
tamleduc <Sakursaoyran204>
2020-11-02 17:13:37 +0700

    tao model question+category+answer

--- src/main/java/com/hm2t/quizbugs/model/questions/Category.java
+++ src/main/java/com/hm2t/quizbugs/model/questions/Category.java
@@ -6,6 +6,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @Entity
 @Data
@@ -13,5 +15,8 @@ public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
+    @NotBlank
+    @NotNull
     private String category;
 }
