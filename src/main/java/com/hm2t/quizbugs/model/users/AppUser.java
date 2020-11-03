24700fb32f593a6c337edfb464656ba9c101e9a4
huy8895 <huy8895@gmail.com>
2020-11-03 23:36:10 +0700

    validator app-user, app-role

--- src/main/java/com/hm2t/quizbugs/model/users/AppUser.java
+++ src/main/java/com/hm2t/quizbugs/model/users/AppUser.java
@@ -1,15 +1,20 @@
 package com.hm2t.quizbugs.model.users;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.Set;
 
 @Entity
 @Data
 @Table(name = "user")
+@AllArgsConstructor
+@NoArgsConstructor
 public class AppUser {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -17,11 +22,13 @@ public class AppUser {
 
     @NotBlank
     @NotNull
+    @Size(min = 5, max = 50)
     @Column(nullable = false, unique = true)
     private String username;
 
     @NotBlank
     @NotNull
+    @Size(min = 5, max = 50)
     private String password;
 
     @ManyToMany(fetch = FetchType.EAGER)
