f71d760b7334cc76d059945be3a1000f0260badd
conaldev <talaaidoaibiet@gmail.com>
2020-11-03 14:28:46 +0700

    them entity -repository - service category

--- src/main/java/com/hm2t/quizbugs/repository/CategoryRepository.java
+++ src/main/java/com/hm2t/quizbugs/repository/CategoryRepository.java
@@ -1,9 +1,9 @@
 package com.hm2t.quizbugs.repository;
 
-import com.hm2t.quizbugs.model.users.AppUser;
+import com.hm2t.quizbugs.model.questions.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<Long, AppUser> {
+public interface CategoryRepository extends JpaRepository<Long, Category> {
 }
---
+++ src/main/java/com/hm2t/quizbugs/service/questions/CategoryService.java
@@ -0,0 +1,7 @@
+package com.hm2t.quizbugs.service.questions;
+
+import com.hm2t.quizbugs.model.questions.Category;
+import com.hm2t.quizbugs.service.IService;
+
+public interface CategoryService extends IService<Category> {
+}
