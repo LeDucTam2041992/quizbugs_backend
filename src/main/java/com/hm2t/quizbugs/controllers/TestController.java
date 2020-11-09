4106f40e7073b31e2a4957affe98a6c32bcd31bc
huy8895 <huy8895@gmail.com>
2020-11-09 22:47:33 +0700

    UpdatePassword - sua lai controller, them 1 model UpdatePassword.java de binding data tu frontend.

---
+++ src/main/java/com/hm2t/quizbugs/controllers/TestController.java
@@ -0,0 +1,44 @@
+package com.hm2t.quizbugs.controllers;
+
+import com.hm2t.quizbugs.model.Test.Test;
+import com.hm2t.quizbugs.model.Test.UserAnswer;
+import com.hm2t.quizbugs.model.Test.UserTest;
+import com.hm2t.quizbugs.service.Test.impl.TestServiceImpl;
+import com.hm2t.quizbugs.service.Test.impl.UserAnswerServiceImpl;
+import com.hm2t.quizbugs.service.Test.impl.UserTestServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/tests")
+public class TestController {
+    @Autowired
+    TestServiceImpl testService;
+
+    @Autowired
+    UserTestServiceImpl userTestService;
+
+    @Autowired
+    UserAnswerServiceImpl userAnswerService;
+
+    @GetMapping
+    public Iterable<Test> getAllTests(){
+        return testService.findAll();
+    }
+
+    @PostMapping
+    public Test createTest(@RequestBody Test test){
+        return testService.save(test);
+    }
+
+    @GetMapping("/userTest")
+    public Iterable<UserTest> getAllUserTests(){
+        return userTestService.findAll();
+    }
+
+    @PostMapping("/userTest")
+    public UserTest createAnswer(@RequestBody UserTest userTest){
+        UserTest userTest1 = userTestService.save(userTest);
+        return userTest1;
+    }
+}
