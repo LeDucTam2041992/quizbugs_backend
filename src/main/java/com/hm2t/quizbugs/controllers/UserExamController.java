af2ae5930a03192e4f4c8227fea19ad61edd6f9b
conaldev <talaaidoaibiet@gmail.com>
2020-11-12 11:31:23 +0700

    cham diem checkboxes cong tru ok

--- src/main/java/com/hm2t/quizbugs/controllers/UserExamController.java
+++ src/main/java/com/hm2t/quizbugs/controllers/UserExamController.java
@@ -26,10 +26,10 @@ import java.util.Set;
 @RestController
 @RequestMapping("/userExams")
 public class UserExamController {
-   private final double MARK =10;
+    private final double MARK = 10;
 
 
-   private Set<UserAnswer> userAnswers;
+    private Set<UserAnswer> userAnswers;
 
     @Autowired
     UserExamServiceImpl userExamService;
@@ -44,61 +44,63 @@ public class UserExamController {
     ExamServiceImpl examService;
 
 
-
     @GetMapping
-    @Secured({"ROLE_ADMIN","ROLE_USER"})
-    public ResponseEntity<Iterable<UserExam>> getExamResultOfUser(){
+    @Secured({"ROLE_ADMIN", "ROLE_USER"})
+    public ResponseEntity<Iterable<UserExam>> getExamResultOfUser() {
         Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        if (principal instanceof UserDetails){
+        if (principal instanceof UserDetails) {
             AppUser curUser = userService.findByUsername(((UserDetails) principal).getUsername());
             Iterable<UserExam> allByUser = userExamService.findAllByUser(curUser);
-            return new ResponseEntity<>(allByUser,HttpStatus.OK);
+            return new ResponseEntity<>(allByUser, HttpStatus.OK);
         }
-        return new ResponseEntity<>(null,HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
     }
 
     @PostMapping
-    @Secured({"ROLE_ADMIN","ROLE_USER"})
-    public ResponseEntity<UserExam> createExamForUser(@RequestBody UserExam userExam){
+    @Secured({"ROLE_ADMIN", "ROLE_USER"})
+    public ResponseEntity<UserExam> createExamForUser(@RequestBody UserExam userExam) {
         Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         AppUser currentUser = userService.findByUsername(((UserDetails) principal).getUsername());
         Optional<Exam> currentExam = examService.findById(userExam.getExam().getId());
         Set<UserAnswer> userAnswers = userExam.getUserAnswers();
         double lengthQuestions = currentExam.get().getQuestionSet().size();
-        double OneTrueQuestion = 1/lengthQuestions;
+        double OneTrueQuestion = 1 / lengthQuestions;
         double userPoint = 0;
         for (UserAnswer uA : userAnswers) {
             Optional<Answer> answer = answerService.findById(uA.getAnswer().getId());
-            if(answer.get().isStatus()){
-                Question question = answer.get().getQuestion();
-                boolean onlyTrue = question.getType() == 2 || question.getType() == 0;
-                if(onlyTrue) {
-                    userPoint+= OneTrueQuestion;
-                } else {
-                    int truePoint = 0;
-                    Question currentQuestion = answer.get().getQuestion();
-                    for(Answer a: currentQuestion.getAnswers()){
-                        if(a.isStatus())
-                            truePoint++;
-                    }
-                    userPoint+=(1/truePoint)/OneTrueQuestion;
+            Question question = answer.get().getQuestion();
+            boolean onlyTrue = question.getType() == 2 || question.getType() == 0;
+            if (onlyTrue) {
+                if (answer.get().isStatus())
+                    userPoint += OneTrueQuestion;
+            } else {
+                double truePoint = 0;
+                double wrongAnswer = 0;
+                Question currentQuestion = answer.get().getQuestion();
+                for (Answer a : currentQuestion.getAnswers()) {
+                    if (a.isStatus())
+                        truePoint++;
                 }
+                if (answer.get().isStatus())
+                    userPoint += (1 / truePoint) / OneTrueQuestion;
+                else
+                    userPoint -= (1 / truePoint) / OneTrueQuestion;
             }
         }
         userExam.setMark(userPoint);
         userExam.setUser(currentUser);
         return new ResponseEntity<>(userExamService.save(userExam),HttpStatus.OK);
-    }
+}
 
     @GetMapping("/getAll")
     @Secured({"ROLE_ADMIN"})
-    public ResponseEntity<?> getAllDoneExam(){
-        return new ResponseEntity<>(userExamService.findAll(),HttpStatus.OK);
+    public ResponseEntity<?> getAllDoneExam() {
+        return new ResponseEntity<>(userExamService.findAll(), HttpStatus.OK);
     }
 
     @GetMapping("{id}")
     @Secured({"ROLE_ADMIN"})
-    public ResponseEntity<?> getUserExamById(@PathVariable("id") Long id){
-        return new ResponseEntity<>(userExamService.findById(id),HttpStatus.OK);
+    public ResponseEntity<?> getUserExamById(@PathVariable("id") Long id) {
+        return new ResponseEntity<>(userExamService.findById(id), HttpStatus.OK);
     }
 }
